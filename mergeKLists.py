e1096011028c39568c8fcf6c62405a556c1a9cba
chien314 <44957569+chien314@users.noreply.github.com>
2022-01-28 07:46:10 -0600

    Update mergeKLists.py

--- mergeKLists.py
+++ mergeKLists.py
@@ -20,3 +20,15 @@ class Solution(object):
         return head.next 
  
   
+class Solution(object):
+    def mergeKLists(self, lists):
+        self.nodes = []
+        head = point = ListNode(0)
+        for l in lists:
+            while l:
+                self.nodes.append(l.val)
+                l = l.next
+        for x in sorted(self.nodes):
+            point.next = ListNode(x)
+            point = point.next
+        return head.next
