dc7d94235e98958dd99f7cf10d4d7c657008151e
chien314 <44957569+chien314@users.noreply.github.com>
2022-02-02 21:16:18 -0600

    Update mergeIntervals.py

--- mergeIntervals.py
+++ mergeIntervals.py
@@ -4,15 +4,25 @@
 
 #  intervals = [[1,3],[2,6],[8,10],[15,18]]
   
-  class Solution:
+class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-      intervals.sort(key=lambda x: x[0])
-      
-      prev_s, prev_e = intervals[0]
-      res = []
-      for i in range(1, len(intervals)):
-        curr_s, curr_e = intervals[i]
-        if prev_e > curr_s:
-          intervals[i] = prev_s, max(prev_e, curr_e)
-          res.append()
-          
+        intervals.sort( key= lambda x:x[0])
+
+        currI=intervals[0]
+        res=[currI]        
+        # res.append()
+
+        # for nextI in intervals:
+        for i in range(1,len(intervals)):
+            nextI=intervals[i]
+            currS, currE = currI
+            nextS, nextE = nextI
+            
+            if currE>=nextS:
+                currI[1]=max(currE,nextE)
+
+            else:
+                
+                res.append(nextI)
+                currI=nextI
+        return res
