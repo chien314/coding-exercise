166de336209135dce321ca74548d8b96d0dd28e7
chien314 <44957569+chien314@users.noreply.github.com>
2022-02-09 21:53:48 -0600

    Create rotateImage.py

---
+++ rotateImage.py
@@ -0,0 +1,19 @@
+# TC:O(n^2)
+# SC:O(1)
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        self.transpose(matrix)
+        self.reflect(matrix)
+    
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n-j-1 ] = matrix[i][n-j-1], matrix[i][j]
+                
