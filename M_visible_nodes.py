0b99ae73bbe51cb53e88c9e58c2b5e4325f71a94
chien314 <44957569+chien314@users.noreply.github.com>
2022-02-19 11:16:11 -0600

    Create M_visible_nodes.py

---
+++ M_visible_nodes.py
@@ -0,0 +1,29 @@
+# TC:O(num of nodes)
+# SC:O(height of tree)
+
+import math
+# Add any extra import statements you may need here
+
+
+class TreeNode: 
+  def __init__(self,key): 
+    self.left = None
+    self.right = None
+    self.val = key 
+
+# Add any helper functions you may need here
+
+
+def visible_nodes(root):
+  # Write your code here
+    def helper(root, level):
+      # base case
+      if root is None:
+        return level
+    
+      L = helper(root.left, level+1)
+      R = helper(root.right, level+1)
+      
+      return max(L,R)
+    
+    return helper(root, 0)
