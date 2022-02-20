128a135e1981af7a3f4294834359f36e242ba082
chien314 <44957569+chien314@users.noreply.github.com>
2022-02-20 11:07:43 -0600

    Create M_queue_removals.py

---
+++ M_queue_removals.py
@@ -0,0 +1,34 @@
+TC:O(x^2)
+SC:O(n)
+
+import math
+from collections import deque 
+
+def findPositions(arr, x):
+  # Write your code here
+  q = deque([(n,i+1) for i,n in enumerate(arr)])
+  ret = []
+  
+  for _ in range(x):
+    mx = float('-inf')
+    popped = []
+    
+    j = 0
+    while q and j < x:
+      n,i = q.popleft()
+      if n > mx:
+        mx = n
+        mxj = j
+        mxi = i
+      popped.append((max(0, n-1), i))
+      j += 1
+      
+      
+    if popped:
+      popped.pop(mxj)
+    if popped:
+      q.extend(popped)
+    
+    ret.append(mxi)
+    
+  return ret 
