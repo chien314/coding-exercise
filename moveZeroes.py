5d28efb687430aa2f24ea006bb2cb29a0c5fc25e
chien314 <44957569+chien314@users.noreply.github.com>
2022-02-23 18:24:16 -0600

    Create moveZeroes.py

---
+++ moveZeroes.py
@@ -0,0 +1,13 @@
+# TC:O(n)
+# SC:O(1)
+
+class Solution:
+    def moveZeroes(self, nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 0                           # both i and j start wtih 0, swap if num[j]!=0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[j],nums[i]=nums[i],nums[j]   # trivial swap: i is either the same position as j or at 0
+                i+=1
