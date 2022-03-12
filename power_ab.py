fc9d4258d6b05c9b2c7e727ed1ae8ae86ec481a4
chien314 <44957569+chien314@users.noreply.github.com>
2022-03-12 15:08:42 -0600

    Update power_ab.py

--- power_ab.py
+++ power_ab.py
@@ -1,3 +1,24 @@
+#best solution: T(log_2 N) and S(1)
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n==0:
+            return 1
+        # if n==1:
+        #     return x
+        if n<0:
+            n=-n
+            x=1/x
+    
+        ans=1
+        while n > 0:
+            if n%2==1:
+                ans=ans*x
+            x=x*x
+            n=n//2  
+            
+        return ans
+      
+# T(log_2 N) and S(log_2 N)      
 class Solution(object):
   def power(self, a, b):
     """
