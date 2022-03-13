e5868431f7a780887416a7a1f0f845c275d342e8
chien314 <44957569+chien314@users.noreply.github.com>
2022-03-13 15:23:59 -0500

    Update A_MWS.py

--- A_MWS.py
+++ A_MWS.py
@@ -1,20 +1,19 @@
 # s = "ADOBECODEBANC", t = "ABC", return "BANC"
 # TC:O(max(m,n)), where m=len(s) and n=len(t)
 # SC:O(max(m,n))
-  
-      i,j
+from collections import Counter
 def MWS(s,t):
-  from collections import Counter
-  dic, lent = collections.Counter(t), len(t)
-  i = I = J = 0
-  for j, c in enumerate(s,1):
-      lent -= dic[c] > 0     # dic(t)-dic(s)
-      dic[c] -= 1
-      if lent==0:
-          while i<j and dic[s[i]] < 0: # i<j?
-              dic[s[i]] += 1
-              i += 1
-          if J==0 or j - i <= J - I:
-              I, J = i, j
-  return s[I:J]     
+    lent, dic = len(t), Counter(t)
+    I=J=i=0
+    for j,c in enumerate(s,1):
+        lent -= dic[c] > 0    # if dic[c]>0: lent-=1
+                dic[c]-=1
+          
+        if lent == 0:
+            while dic[s[i]]<0:  # i<j?
+                dic[s[i]]+=1
+                i+=1
+            if J==0 or J-I > j-i:
+                I,J =i,j 
+    return s[I:J]  
         
