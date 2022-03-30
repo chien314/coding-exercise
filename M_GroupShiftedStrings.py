884d1aed52e1ac9aecea49fa70637fd6d7130f3a
chien314 <44957569+chien314@users.noreply.github.com>
2022-03-29 20:33:03 -0500

    Update M_GroupShiftedStrings.py

--- M_GroupShiftedStrings.py
+++ M_GroupShiftedStrings.py
@@ -1,4 +1,7 @@
 # TC:O(k*n) =SC, where n be the length of strings and k be the maximum length of a string in strings
+# note that: 
+# C++/Java: a % b = a - int(a / b) * b
+# Python: a % b = a - floor(a / b) * b
 
 def groupStrings(self, strings):
     groups = collections.defaultdict(list)
