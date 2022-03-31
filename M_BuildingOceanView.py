243227997d0fe6f5f51499dd23fc34184f5dc1c4
chien314 <44957569+chien314@users.noreply.github.com>
2022-03-31 07:23:43 -0500

    Update M_BuildingOceanView.py

--- M_BuildingOceanView.py
+++ M_BuildingOceanView.py
@@ -3,7 +3,7 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
         mx=heights[-1]
-        res=[len(heights)-1]
+        res=[len(heights)-1] 
         for i in reversed(range(len(heights)-1)):
             if heights[i]>mx:
                 res.append(i)
