ca40be7e090b9470833b34ac92457cebc38d85f5
chien314 <44957569+chien314@users.noreply.github.com>
2022-04-10 08:15:21 -0500

    Create permute.py

---
+++ permute.py
@@ -0,0 +1,19 @@
+# TC:O(N * N!) = SC
+class Solution:
+  def permute(self, nums):
+    
+    res=[]
+    def bt(first):
+           
+       if first ==len(nums):
+          res.append(nums.copy())
+
+       for i in range(first, len(nums)):
+          nums[first], nums[i] = nums[i], nums[first]
+          bt(first+1)
+          nums[first], nums[i] = nums[i], nums[first]
+       return
+    bt(0)
+    return res
+    
+            
