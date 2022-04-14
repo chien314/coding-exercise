dcb92e5d60c9e44c445baf459f6f87f918c0f14b
chien314 <44957569+chien314@users.noreply.github.com>
2022-04-14 06:48:50 -0500

    Update M_diameter.py

--- M_diameter.py
+++ M_diameter.py
@@ -17,7 +17,76 @@ class Solution:
             return max(L,R)+1
         
         diameter=0
-      
-        
+
         dfs(root)
         return diameter
+
+# case 2
+#1522
+class Solution:
+    def diameter(self, root: 'Node') -> int:
+        """
+        :type root: 'Node'
+        :rtype: int
+        """
+        def dfs(root):
+            nonlocal diameter
+            maxd1 = maxd2 = 0
+            for child in root.children:
+                d = dfs(child)
+                if d > maxd1:
+                    maxd2, maxd1 = maxd1, d
+                elif d > maxd2:
+                    maxd2 = d
+            diameter = max(diameter, maxd1+maxd2)
+            return maxd1 + 1
+        
+        diameter = 0
+        dfs(root)
+        return diameter
+    
+# case 3
+#1245
+class Solution:
+    def treeDiameter(self, edges: List[List[int]]) -> int:
+        def dfs(root, parent):
+            nonlocal diameter
+            maxd1 = maxd2 = 0 # 2 max depth branches
+            for child in graph[root]:
+                if child != parent: # only one way traversal is allowed, dont go back to parent
+                    d = dfs(child, root)
+                    if d > maxd1:
+                        maxd2, maxd1 = maxd1, d
+                    elif d > maxd2:
+                        maxd2 = d
+            diameter = max(diameter, maxd1+maxd2)
+            return maxd1+1
+        
+        if not edges:
+            return 0
+        
+        graph = collections.defaultdict(list)
+        for i, j in edges:
+            graph[i].append(j)
+            graph[j].append(i)
+            
+        diameter = 0
+        dfs(0, -1)
+        return diameter
+    
+# case 4
+#124
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int
+        def dfs(root):
+            nonlocal maxsum
+            if not root:
+                return 0
+            ls, rs = dfs(root.left), dfs(root.right)
+            maxsum = max(maxsum, root.val+max(0,ls)+max(0,rs))
+            return root.val + max(0, ls, rs)
+            
+        maxsum = float('-inf')
+        dfs(root)
+        return maxsum
+    
