f2d11afdb92c03aafa75617ed0ddb8a1c57d988e
chien314 <44957569+chien314@users.noreply.github.com>
2022-04-13 19:51:53 -0500

    Update M_subarraySumEqk.py

--- M_subarraySumEqk.py
+++ M_subarraySumEqk.py
@@ -16,3 +16,28 @@ class Solution:
             
             dic[run_sum] +=1 # 
         return total
+
+    
+# case 2: TC:O(n), SC:O(n)    
+# maxSubArrayLen    
+class Solution:
+    def maxSubArrayLen(self, nums: List[int], k: int) -> int:
+        # [1,-1,5,-2,3]
+        # run_sum = [1,0,5,3,6], k=3
+        # 
+        dic = defaultdict(lambda: 0)
+        dic[0]=-1
+        mx =0
+        run_sum=0
+        
+        for i, x in enumerate(nums):
+            run_sum+=x
+            # run_sum - su = k
+            su = run_sum -k
+            if su in dic:
+                if i-dic[su] > mx:
+                    mx = i-dic[su]
+            if run_sum not in dic:
+                dic[run_sum] = i
+        
+        return mx     
